09fc9a35ced0828367af5f0e2212af7cb642cdce
Rais Iqbal <bahshat@gmail.com>
2024-02-11 20:44:51 +0800

    Live API URL added

--- src/data/data.ts
+++ src/data/data.ts
@@ -1,13 +1,28 @@
 // dataService.js
 
+const fetchLocally = async () => {
+  try {
+    const response = await fetch("./data/question.json");
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    return null;
+  }
+};
+
+const fetchFromServer = async () => {
+  try {
+    const response = await fetch("https://knowledge-backend.onrender.com/api/questions");
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    return null;
+  }
+};
+
 export const fetchData = async () => {
-    try {
-      const response = await fetch("./data/question.json");
-      const data = await response.json();
-      return data;
-    } catch (error) {
-      console.error("Error fetching data:", error);
-      return null;
-    }
-  };
-  
+  const data = await fetchFromServer();
+  return data;
+};
